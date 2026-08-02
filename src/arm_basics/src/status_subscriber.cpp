f1d92e1b50bafb4f44503444175666c4ff506be2
linxi <linxier072@gmail.com>
2026-08-02 13:45:58 +0800

    Add arm status subscriber

---
+++ src/arm_basics/src/status_subscriber.cpp
@@ -0,0 +1,41 @@
+#include <functional>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+class StatusSubscriber : public rclcpp::Node
+{
+public:
+  StatusSubscriber()
+  : Node("arm_status_subscriber")
+  {
+    subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "/arm_status",
+      10,
+      std::bind(
+        &StatusSubscriber::status_callback,
+        this,
+        std::placeholders::_1));
+  }
+
+private:
+  void status_callback(
+    const std_msgs::msg::String::SharedPtr message) const
+  {
+    RCLCPP_INFO(
+      this->get_logger(),
+      "Arm status received: '%s'",
+      message->data.c_str());
+  }
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<StatusSubscriber>());
+  rclcpp::shutdown();
+  return 0;
+}
